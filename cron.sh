bac2bde4450e7452bc829417b1dd94dfcb95e3a0
cglotr <aaryfn@gmail.com>
2023-07-16 16:02:10 -0700

    reset to upstream first

--- cron.sh
+++ cron.sh
@@ -14,12 +14,14 @@ log_info() {
 }
 
 while true; do
+    run_cmd "git fetch"
+    run_cmd "git reset --hard origin/main"
+
     log_info "running parser"
     run_cmd "$PARSER_BIN --pdf-dir=$PARSER_PDF_DIR --out-dir=$PARSER_OUT"
     log_info "parser done"
 
     log_info "pushing update"
-    run_cmd "git reset --hard origin/main"
     run_cmd "git add -A"
     run_cmd "git commit -m 'kehadiran auto update'"
     run_cmd "git push"
